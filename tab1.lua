78722ecb18f8bdaaa0965118872e2cdf52cc5b05
voquochung2369-creator <voquochung2369@gmail.com>
2026-08-18 14:58:21 +0800

    tab1.lua

---
+++ tab1.lua
@@ -0,0 +1,21 @@
+local Window = _G.CustomHubWindow
+
+
+local Bloxfruit = Window:MakeTab({
+
+    Name = "Blox Fruit"
+
+})
+
+
+Bloxfruit:AddButton({
+
+    Name = "Real Kid",
+
+    Callback = function()
+
+        print("Real Kid")
+
+    end
+
+})
